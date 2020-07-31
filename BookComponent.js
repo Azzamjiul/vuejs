4db98399ca071b96b622e7246cfaff1356b90e87
azzam <azzam.jiul@gmail.com>
2020-07-31 16:33:10 +0700

    first commit

---
+++ BookComponent.js
@@ -0,0 +1,15 @@
+export const BookComponent = {
+    data () {
+        return {
+            classCard: 'card'
+        }
+    },
+    props: ['book'],
+    template: `
+        <div :class="classCard">
+            <h3>{{ book.title }}</h3>
+            <p v-html="book.description"></p>
+            <button @click="$emit('pilih', book.title)"> Select</button>
+        </div>
+    `
+}
